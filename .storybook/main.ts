cd4d05af8b20640e7256bccc171bf560cac9ab2e
fullmoon <1317812522@qq.com>
2024-12-14 21:21:51 +0800

    feat: 新增 scrollbar 组件

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -8,6 +8,41 @@ const config: StorybookConfig = {
     '@storybook/addon-essentials',
     '@chromatic-com/storybook',
     '@storybook/addon-interactions',
+    {
+      name: '@storybook/addon-styling-webpack',
+      options: {
+        rules: [
+          {
+            test: /\.css$/,
+            use: [
+              'style-loader',
+              {
+                loader: 'css-loader',
+                options: {
+                  modules: {
+                    auto: true,
+                    localIdentName: '[hash:base64:5]',
+                  },
+                },
+              },
+            ],
+          },
+          {
+            test: /\.less$/i,
+            use: [
+              'style-loader',
+              'css-loader',
+              {
+                loader: 'less-loader',
+                options: {
+                  implementation: require.resolve('less'),
+                },
+              },
+            ],
+          },
+        ],
+      },
+    },
   ],
   framework: {
     name: '@storybook/nextjs',
--- packages/react-components/stories/scrollbar.stories.tsx
+++ packages/react-components/stories/scrollbar.stories.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import type { Meta } from '@storybook/react';
-import { Scrollbar } from '../src/scrollbar';
+import { Scrollbar } from '../src/scrollbar/Scrollbar';
 
 const meta = {
   title: 'React Components/Scrollbar',
@@ -8,6 +8,28 @@ const meta = {
 
 export default meta;
 
-export const Demo = () => {
-  return <Scrollbar />;
-};
+export const Basic = () => (
+  <div style={{ height: '390px', width: '440px', backgroundColor: '#f0f0f0' }}>
+    <Scrollbar>
+      {Array.from({ length: 200 }).map((_, index) => (
+        <div key={index} style={{ padding: '10px', marginBottom: '2px', whiteSpace: 'nowrap' }}>
+          Item {index + 1} 那只是一场游戏一场梦
+        </div>
+      ))}
+    </Scrollbar>
+  </div>
+);
+
+export const Horizontal = () => (
+  <div style={{ height: '390px', width: '440px', backgroundColor: '#f0f0f0' }}>
+    <Scrollbar>
+      {Array.from({ length: 6 }).map((_, index) => (
+        <div key={index} style={{ padding: '10px', marginBottom: '2px', whiteSpace: 'nowrap' }}>
+          Item {index + 1} 那只是一场游戏一场梦 - 这是一段很长的文字，
+          这是一段很长的文字这是一段很长的文字这是一段很长的文字这是一段很长的文字这是一段很长的文字这是一段很长的文字
+          用来测试水平滚动条 那只是一场游戏一场梦 - 这是一段很长的文字，用来测试水平滚动条
+        </div>
+      ))}
+    </Scrollbar>
+  </div>
+);
